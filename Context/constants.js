b871688c50c47619a6073ccab0e2ebabee8a62f9
K2302 <95227135+K2302@users.noreply.github.com>
2023-04-06 19:52:54 +0530

    Nice

---
+++ Context/constants.js
@@ -0,0 +1,7 @@
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+import chatApp from "./ChatApp.json";
+
+export const ChatAppAddress = "0xB274a1939a539F18ceeb864bf7e36d045E235627";
+
+export const ChatAppABI = chatApp.abi;
+
